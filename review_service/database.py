6d4b9055354964b8cd9c4ac28a456d3956d5640f
nikhil1209 <nikhilsai.thentu@psiog.com>
2025-01-19 21:05:56 +0530

    Initial commit

--- review_service/database.py
+++
@@ -1,9 +0,0 @@
-# review_service/db.py
-from beanie import init_beanie
-from motor.motor_asyncio import AsyncIOMotorClient
-from review_service.models import Review  # Import your Beanie model
-
-async def initiate_db():
-    client = AsyncIOMotorClient("mongodb+srv://nikhilthentu:r9w0u4OZDx2M4Eeh@cluster0.wgn3c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # Replace with your MongoDB Atlas URI
-    database = client.get_database("food_Db")   # Replace with your database name
-    await init_beanie(database, document_models=[Review])  # Initialize Beanie with your models
--- user_service/models.py
+++
@@ -1,8 +0,0 @@
-from beanie import Document
-
-class User(Document):
-    email: str
-    name: str
-
-    class Settings:
-        collection = "users"
